40fba34619863a60304941384dd4ba3cf392a0e5
OneBogdan01 <bogdan.game.development@gmail.com>
2021-02-20 23:20:55 +0200

    Minor on Feedback

---
+++ Trees vs Insects/Assets/DevugLowFramerate.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DevugLowFramerate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void To10 (int fr)
+    {
+        Application.targetFrameRate = fr;
+    }
+
+    public void ToInfinite ()
+    {
+        Application.targetFrameRate = -1;
+    }
+}
