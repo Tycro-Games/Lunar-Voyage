452a9877596966fb96177429dc7f1889725198d5
NiceGuy <bogdan.game.development@gmail.com>
2021-06-14 15:10:18 +0300

    Mine animation template

--- Trees vs Insects/Assets/Scripts/Tree/TreeModules/DestroyTree.cs
+++ Trees vs Insects/Assets/Scripts/Tree/TreeModules/DestroyTree.cs
@@ -12,21 +12,31 @@ namespace Bogadanul.Assets.Scripts.Tree
 
         public virtual void DestroyTheTree()
         {
-
             StartCoroutine(Destroy());
-           
         }
+
+        public virtual void DestroyTheTreeOntime(float time)
+        {
+            StartCoroutine(Destroy(time));
+        }
+
         private IEnumerator Destroy()
         {
             gameObject.SetActive(false);
-            Destroy(gameObject,2);
+            Destroy(gameObject, 2);
             EnemyManager.SetSpace();
             yield return null;
-           
-            
-            
-
         }
+
+        private IEnumerator Destroy(float time)
+        {
+            yield return new WaitForSeconds(time);
+            gameObject.SetActive(false);
+            Destroy(gameObject, 2);
+            EnemyManager.SetSpace();
+            yield return null;
+        }
+
         public virtual void TakeDG(int dg)
         {
             hp -= dg;
